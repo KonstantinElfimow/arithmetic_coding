69815d36238f7b5a3d5501c5458c9034954b6eaf
Konstantin <k.elfimow@gmail.com>
2022-12-05 11:36:42 +0300

    Порефакторил код

--- arithmetic_coding.py
+++ arithmetic_coding.py
@@ -4,6 +4,80 @@ from math import ceil
 accurateness: int = 8  # Округдение до знака
 
 
+def _insert_row_with_format(table: list, l: list = None, length: int = 0) -> None:
+    if l:
+        table.append(tuple(l))
+    if length:
+        table.append(tuple(['-' * length]))
+
+
+def _create_encode_table(p_ensemble: dict, q_ensemble: dict, seq: list) -> (tuple, float, float):
+    F_ik = 0
+    G_ik = 1
+
+    encode_table: list = list()
+    _insert_row_with_format(encode_table, ['step k', 's_i', 'p(s_i)', 'q(s_i)', 'F(s_ik)', 'G(s_ik)'], 65)
+
+    _insert_row_with_format(encode_table, [0, '-', '-', '-', F_ik, G_ik], 65)
+
+    for i, alpha in enumerate(seq):
+        if i == 0:
+            F_ik = q_ensemble[alpha]
+            G_ik = p_ensemble[alpha]
+        else:
+            F_ik = round(F_ik + round(G_ik * q_ensemble[alpha], accurateness), accurateness)
+            G_ik = round(G_ik * p_ensemble[alpha], accurateness)
+
+        _insert_row_with_format(encode_table, [i + 1, alpha, p_ensemble[alpha], q_ensemble[alpha], F_ik, G_ik], 65)
+
+    return tuple(encode_table), F_ik, G_ik
+
+
+def _bin_to_float(b: str) -> float:
+    return round(sum([int(x) * 2 ** -(i + 1) for i, x in enumerate(b)]), accurateness)
+
+
+def _create_decode_table(p_ensemble: dict, q_ensemble: dict, seq: list, code_word: str) -> tuple:
+    F_k = 0
+    G_k = 1
+
+    decode_table: list = list()
+    _insert_row_with_format(decode_table, ['step k', 'F_k', 'G_k', 'Guess s_i', 'q(s_i)', 'Comparison', 'Solution'], 90)
+
+    x: float = _bin_to_float(code_word)
+    _insert_row_with_format(decode_table, [0, 'X = 0.{} -> x = {}'.format(code_word, x)], 90)
+
+    for i, alpha in enumerate(seq):
+        for s, q in q_ensemble.items():
+            _insert_row_with_format(decode_table, [i + 1, F_k, G_k, s, q, f'{round(F_k + q * G_k, accurateness)} < {x}',
+                                                   round(F_k + q * G_k, accurateness) < x])
+        _insert_row_with_format(decode_table, length=90)
+        if i == 0:
+            F_k = q_ensemble[alpha]
+            G_k = p_ensemble[alpha]
+        else:
+            F_k = round(F_k + round(G_k * q_ensemble[alpha], accurateness), accurateness)
+            G_k = round(G_k * p_ensemble[alpha], accurateness)
+    return tuple(decode_table)
+
+
+def _make_q_ensemble(p_ensemble: dict) -> dict:
+    q_ensemble: dict = dict()
+    q: float = 0.0
+    for i, (alpha, p) in enumerate(p_ensemble.items()):
+        q_ensemble[alpha] = q
+        q = (round(q + p, accurateness))
+    return q_ensemble
+
+
+def _length_of_code_word(G: float) -> int:
+    return ceil(- log2(G)) + 1
+
+
+def _code_word_digital(F: float, G: float) -> float:
+    return round(F + round(G / 2, accurateness), accurateness)
+
+
 def _float_to_bin(f: float) -> str:
     result: str = ''
     inter: float = f
@@ -17,64 +91,16 @@ def _float_to_bin(f: float) -> str:
     return result
 
 
-def _create_encode_table(p_ensemble: dict, q_ensemble: dict, seq: list) -> (tuple, float, float):
-    F_ik: float = 0.0
-    G_ik: float = 0.0
-
-    encode_table: list = list()
-    encode_table.append(tuple(['step k', 's_i', 'p(s_i)', 'q(s_i)', 'F(s_ik)', 'G(s_ik)']))
-    encode_table.append(tuple(['-' * 65]))
-    encode_table.append(tuple([0, '-', '-', '-', 0, 1]))
-    encode_table.append(tuple(['-' * 65]))
-
-    for i, alpha in enumerate(seq):
-        if i == 0:
-            F_ik = q_ensemble[alpha]
-            G_ik = p_ensemble[alpha]
-        else:
-            F_ik = round(F_ik + round(G_ik * q_ensemble[alpha], accurateness), accurateness)
-            G_ik = round(G_ik * p_ensemble[alpha], accurateness)
-        encode_table.append(tuple([i + 1, alpha, p_ensemble[alpha], q_ensemble[alpha], F_ik, G_ik]))
-        encode_table.append(tuple(['-' * 65]))
-    return tuple(encode_table), F_ik, G_ik
-
-
-def create_decode_table(p_ensemble: dict, q_ensemble: dict, seq: list, code_word: str) -> tuple:
-    decode_table: list = list()
-    decode_table.append(tuple(['step k', 'F_k', 'G_k', 'Guess s_i', 'q(s_i)', 'Comparison', 'Solution']))
-    decode_table.append(tuple(['-' * 90]))
-    x: float = round(sum([int(x) * 2 ** -(i + 1) for i, x in enumerate(code_word)]), accurateness)
-    decode_table.append(tuple([0, 'X = 0.{} -> x = {}'.format(code_word, x)]))
-    decode_table.append(tuple(['-' * 90]))
-
-    F_k: float = 0.0
-    G_k: float = 1.0
-    for i, alpha in enumerate(seq):
-        for s, q in q_ensemble.items():
-            decode_table.append(tuple([i + 1, F_k, G_k, s, q, f'{round(F_k + q * G_k, accurateness)} < {x}', round(F_k + q * G_k, accurateness) < x]))
-        decode_table.append(tuple(['-' * 90]))
-        if i == 0:
-            F_k = q_ensemble[alpha]
-            G_k = p_ensemble[alpha]
-        else:
-            F_k = round(F_k + round(G_k * q_ensemble[alpha], accurateness), accurateness)
-            G_k = round(G_k * p_ensemble[alpha], accurateness)
-    return tuple(decode_table)
-
-
 def _arithmetic_encode_algorythm(p_ensemble: dict, seq: list) -> (tuple, tuple, int, float, str):
-    q_ensemble: dict = dict()
-    q: float = 0.0
-    for i, (alpha, p) in enumerate(p_ensemble.items()):
-        q_ensemble[alpha] = q
-        q = (round(q + p, accurateness))
+    q_ensemble: dict = _make_q_ensemble(p_ensemble)
 
-    encode_table, q_seq_i, p_seq_i = _create_encode_table(p_ensemble, q_ensemble, seq)
-    L: int = ceil(- log2(p_seq_i)) + 1
-    X: float = round(q_seq_i + round(p_seq_i / 2, accurateness), accurateness)
-    code_word = _float_to_bin(X)
+    encode_table, F, G = _create_encode_table(p_ensemble, q_ensemble, seq)
 
-    decode_table = create_decode_table(p_ensemble, q_ensemble, seq, code_word[0: L])
+    L: int = _length_of_code_word(G)
+    X: float = _code_word_digital(F, G)
+    code_word: str = _float_to_bin(X)
+
+    decode_table = _create_decode_table(p_ensemble, q_ensemble, seq, code_word[0: L])
 
     return encode_table, decode_table, L, X, code_word
 
